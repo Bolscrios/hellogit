f5d8b9c9bdfc6950d0ec0233bb3eeb30b5bd0776
Bolscrios <Bolscrios@users.noreply.github.com>
2019-01-30 11:24:07 -0400

    Added source file

---
+++ program_one.cpp
@@ -0,0 +1,16 @@
+//Program which prompts for, and accepts, an age
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+string S0;
+string s1 = "Hello";
+cout << "S0 = " << s0 << endl;
+cout << s1 << endl;
+int age = 0;
+std::cout << "Hello, how old are you?" << endl;
+std::cin >> age;
+return 0;
+}
